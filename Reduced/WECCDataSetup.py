5798dd6bc529a5ec74982197ee51c6b9ba8df40d
Jordan Kern <jkern@ncsu.edu>
2020-09-18 21:46:01 -0400

    update

--- Reduced/WECCDataSetup.py
+++ Reduced/WECCDataSetup.py
@@ -3,6 +3,7 @@ import csv
 import pandas as pd
 import numpy as np
 
+#hello world!
 
 ######=================================================########
 ######               Segment A.1                       ########
