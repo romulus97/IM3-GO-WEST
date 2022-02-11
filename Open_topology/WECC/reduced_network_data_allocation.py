b0e8695d705147af1d6c34a5643cd02b22b7fde9
kakdemir <56842236+keremakdemir@users.noreply.github.com>
2022-02-11 17:50:34 -0500

    typo correction

--- Open_topology/WECC/reduced_network_data_allocation.py
+++ Open_topology/WECC/reduced_network_data_allocation.py
@@ -31,7 +31,7 @@ UC_TREATMENTS = ['_simple']
 # line_limit_scaling = [25,50,75,100]
 line_limit_scaling = [*range(0,150,50)]
 
-congested_line_scaling = [*range(50,950)]
+congested_line_scaling = [*range(50,950,50)]
 
 for NN in NODE_NUMBER:
     
