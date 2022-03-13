d4dbbc7d2383b9ecc77e01fe1fd18143c65e231a
kakdemir <56842236+keremakdemir@users.noreply.github.com>
2022-03-12 20:56:46 -0500

    Time zone adjustment

--- Open_topology/WECC/BA_data/EIA_data.py
+++ Open_topology/WECC/BA_data/EIA_data.py
@@ -6,63 +6,84 @@ Created on Thu Feb 11 22:24:48 2021
 """
 import pandas as pd
 import numpy as np
-import xlrd
 
+#read BA name data and save BA abbreviations
 df_BAs = pd.read_csv('BAs.csv',header=0)
-
-#assign EIA 930 load for 2019 to each BA
 abbr = list(df_BAs['Abbreviation'])
 
 for a in abbr:
-
-    idx = abbr.index(a)
-    filename = '../../CSV_Files/' + a + '_Hourly_Load_Data.csv'
-    b = pd.read_csv(filename,header=0)
-    load = b.loc[b['Year']==2019,'Adjusted_Demand_MWh']
     
-    if idx < 1:
-        C = load
-    else:
-        C = np.column_stack((C,load))
-
-r,c = np.shape(C)
-for i in range(0,r):
-    for j in range(0,c):
-        if C[i,j] < 0:
-            C[i,j] = 0
-df_C = pd.DataFrame(C)
-df_C.columns = abbr
-df_C.to_csv('BA_load.csv')
-
-for a in abbr:
-    
+    #saving BA index and reading hourly demand, solar and wind data for 2019
     idx = abbr.index(a)
     filename = '../../Raw_Data/' + a + '.xlsx'
-    b = pd.read_excel(filename,header=0,sheet_name='Published Hourly Data')
-    solar = b.loc[b['UTC time'].dt.year == 2019,'Adjusted SUN Gen']
-    wind = b.loc[b['UTC time'].dt.year == 2019,'Adjusted WND Gen']
+    renewable_data = pd.read_excel(filename,header=0,sheet_name='Published Hourly Data')
     
-    if idx < 1:
-        S = solar
-        W = wind
+    solar = renewable_data.loc[renewable_data['Local time'].dt.year == 2019,'Adjusted SUN Gen'].copy()
+    wind = renewable_data.loc[renewable_data['Local time'].dt.year == 2019,'Adjusted WND Gen'].copy()
+    
+    #saving the time zone
+    tz = renewable_data.loc[0,'Time zone']
+    
+    #reading hourly load data for 2019
+    filename = '../../CSV_Files/' + a + '_Hourly_Load_Data.csv'
+    load_data = pd.read_csv(filename,header=0)
+    load = load_data.loc[load_data['Year']==2019,'Adjusted_Demand_MWh'].copy()
+    
+    #checking the time zone of the BA and shifting load, solar and wind timeseries accordingly 
+    #(Pacific time zone is selected as standard time zone)
+    
+    if tz == 'Pacific':
+        #copying relevant timeseries as is if time zone is Pacific
+        S = np.array(solar)
+        W = np.array(wind)
+        L = np.array(load)
+    
+    elif tz == 'Mountain' or tz == 'Arizona':
+        #shifting revelant timeseries for 1 hour if time zone is Mountain or Arizona
+        renewable_ind = [*solar.index]
+        fixed_renewable_ind = [c+1 for c in renewable_ind]
+        solar = renewable_data.loc[fixed_renewable_ind,'Adjusted SUN Gen'].copy()
+        wind = renewable_data.loc[fixed_renewable_ind,'Adjusted WND Gen'].copy()
+        S = np.array(solar)
+        W = np.array(wind)
+        
+        load_ind = [*load.index]
+        fixed_load_ind = [c+1 for c in load_ind]
+        load = load_data.loc[fixed_load_ind,'Adjusted_Demand_MWh'].copy()
+        L = np.array(load)
+    
     else:
-        S = np.column_stack((S,solar))
-        W = np.column_stack((W,wind))
-
-r,c = np.shape(S)
-for i in range(0,r):
-    for j in range(0,c):
-        if S[i,j] < 0:
-            S[i,j] = 0
-df_S = pd.DataFrame(S)
-df_S.columns = abbr
+        #if timezone is any of the above, show the BA and time zone
+        print([abbr,tz])
+        
+    #copying all BA level data side by side
+    if idx < 1:
+        S_end = S
+        W_end = W
+        L_end = L
+        
+    else:
+        S_end = np.column_stack((S_end,S))
+        W_end = np.column_stack((W_end,W))
+        L_end = np.column_stack((L_end,L))
+        
+#Turning timeseries into dataframe, changing negative values to 0 and saving the timeseries
+df_S = pd.DataFrame(S_end, columns=abbr)
+df_S[df_S < 0] = 0
 df_S.to_csv('BA_solar.csv')
 
-r,c = np.shape(W)
-for i in range(0,r):
-    for j in range(0,c):
-        if W[i,j] < 0:
-            W[i,j] = 0
-df_W = pd.DataFrame(W)
-df_W.columns = abbr
+df_W = pd.DataFrame(W_end, columns=abbr)
+df_W[df_W < 0] = 0
 df_W.to_csv('BA_wind.csv')
+
+df_L = pd.DataFrame(L_end, columns=abbr)
+df_L[df_L < 0] = 0
+df_L.to_csv('BA_load.csv')
+
+
+
+
+
+
+
+
