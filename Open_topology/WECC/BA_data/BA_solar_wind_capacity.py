d375a173dd4b230689f3dc34a06deb6d2a704d79
kakdemir <56842236+keremakdemir@users.noreply.github.com>
2022-03-31 21:18:53 -0400

    BA hydro fix

--- Open_topology/WECC/BA_data/BA_solar_wind_capacity.py
+++ Open_topology/WECC/BA_data/BA_solar_wind_capacity.py
@@ -14,13 +14,15 @@ plants = pd.read_excel('Plant_EIA860.xlsx',header=1)
 #Reading EIA solar and wind data
 solar = pd.read_excel('Solar_EIA860.xlsx',sheet_name='Operable',header=1)
 wind = pd.read_excel('Wind_EIA860.xlsx',sheet_name='Operable',header=1)
+generator = pd.read_excel('Generator_EIA860.xlsx',sheet_name='Operable',header=1)
+hydro = generator.loc[generator['Technology']=='Conventional Hydroelectric'].copy()
 
 #Reading BA names 
 BAs_data = pd.read_csv('BAs.csv',header=0)
 BAs_abb = list(BAs_data['Abbreviation'])
 
 #Creating a dataframe to store data
-solar_wind_df = pd.DataFrame(np.zeros((len(BAs_data),2)),columns=['Solar','Wind'],index=list(BAs_data['Name']))
+solar_wind_df = pd.DataFrame(np.zeros((len(BAs_data),3)),columns=['Solar','Wind','Hydro'],index=list(BAs_data['Name']))
 
 #summing solar generation capacity for each BA
 for ind,row in solar.iterrows():
@@ -48,6 +50,21 @@ for ind,row in wind.iterrows():
         
         wind_BA_name = BAs_data.loc[BAs_data['Abbreviation']==wind_BA_abb]['Name'].values[0]
         solar_wind_df.loc[wind_BA_name,'Wind'] = solar_wind_df.loc[wind_BA_name,'Wind'] + wind_BA_cap
+       
+    else:
+        pass
+    
+#summing hydro generation capacity for each BA
+for ind,row in hydro.iterrows():
+    
+    code = row['Plant Code']
+    hydro_BA_abb = plants.loc[plants['Plant Code']==code]['Balancing Authority Code'].values[0]
+    hydro_BA_cap = row['Nameplate Capacity (MW)']
+    
+    if hydro_BA_abb in BAs_abb:
+        
+        hydro_BA_name = BAs_data.loc[BAs_data['Abbreviation']==hydro_BA_abb]['Name'].values[0]
+        solar_wind_df.loc[hydro_BA_name,'Hydro'] = solar_wind_df.loc[hydro_BA_name,'Hydro'] + hydro_BA_cap
        
     else:
         pass
